9d37e6f5b8c5c28a586fb44be9ce4680c1d82cb1
wolf <shanxiee@outlook.com>
2022-06-07 18:02:37 +0800

    SetpLockServer

---
+++ Server_/SimpleServer/Src/FakeServer/ServerLauncher.cs
@@ -0,0 +1,14 @@
+
+using System.Diagnostics;
+using Lockstep.Logging;
+using Debug = Lockstep.Logging.Debug;
+
+
+public class ServerLauncher
+{
+    public static void Main()
+    {
+        // Console.WriteLine("Main Start");
+        Debug.Log("Mian start");
+    }
+}
