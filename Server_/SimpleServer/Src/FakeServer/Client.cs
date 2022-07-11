1151243d9a7a6ebf9fd1b93a63356ea8306ea184
wolf <shanxiee@outlook.com>
2022-07-11 11:37:57 +0800

    fakeServer Done

---
+++ Server_/SimpleServer/Src/FakeServer/Client.cs
@@ -0,0 +1,55 @@
+using Lockstep.Network;
+
+namespace Lockstep.FakeServer
+{
+    public class Client : IMessageDispatcher
+    {
+        private NetOuterProxy net = new NetOuterProxy();
+        public Session Session;
+
+        private int count = 0;
+        public int id;
+
+        public void Start()
+        {
+            net.Awake(NetworkProtocol.TCP);
+            net.MessageDispatcher = this;
+            net.MessagePacker = MessagePacker.Instance;
+            Session = net.Create(Server.serverIpPoint);
+            Session.Start();
+        }
+
+        public void Dispatch(Session session, Packet packet)
+        {
+            ushort opcode = packet.Opcode();
+            var message =
+                session.Network.MessagePacker.DeserializeFrom(opcode, packet.Bytes, Packet.Index,
+                    packet.Length - Packet.Index) as IMessage;
+            var type = (EMsgType)opcode;
+            switch (type)
+            {
+                case EMsgType.FrameInput:
+                    OnFrameInput(session, message);
+                    break;
+                case EMsgType.StartGame:
+                    OnStartGame(session, message);
+                    break;
+            }
+        }
+
+        private void OnStartGame(Session session, IMessage message)
+        {
+            var msg = message as Msg_StartGame;
+        }
+
+        private void OnFrameInput(Session session, IMessage message)
+        {
+            var msg = message as Msg_FrameInput;
+        }
+
+        public void Send(IMessage msg)
+        {
+            Session.Send(msg);
+        }
+    }
+}
--- Server_/SimpleServer/Src/FakeServer/ServerLauncher.cs
+++ Server_/SimpleServer/Src/FakeServer/ServerLauncher.cs
@@ -1,13 +1,55 @@
-
 using System.Diagnostics;
 using Lockstep.Logging;
+using Lockstep.Network;
 using Debug = Lockstep.Logging.Debug;
 
-public class ServerLauncher
+
+namespace Lockstep.FakeServer
 {
-    public static void Main()
+    public class ServerLauncher
     {
-        // Console.WriteLine("Main Start");
-        Debug.Log("Mian start");
+        private static Server server;
+
+        public static void Main()
+        {
+            OneThreadSynchronizationContext contex = new OneThreadSynchronizationContext();
+            SynchronizationContext.SetSynchronizationContext(contex);
+            Debug.Log("Mian start");
+            try
+            {
+                DoAwake();
+                while (true)
+                {
+                    try
+                    {
+                        Thread.Sleep(3);
+                        contex.Update();
+                        server.Update();
+                    }
+                    catch (ThreadAbortException e)
+                    {
+                        return;
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error(e.ToString());
+                    }
+                }
+            }
+            catch (ThreadAbortException e)
+            {
+                return;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.ToString());
+            }
+        }
+
+        private static void DoAwake()
+        {
+            server = new Server();
+            server.start();
+        }
     }
 }
